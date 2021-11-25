b533df5de6c8e63dfc3377c6efca1f06e2a97de9
mario1842 <94576554+mario1842@users.noreply.github.com>
2021-11-25 18:32:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ classifiers = [
 ]
  
 setup(
-  name='mariocard',
-  version='1.0.5',
+  name='mariocard.py',
+  version='1.0.0',
   description='This is simple maker for cards in discord bot.',
   long_description=open('README.rst').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='',  
@@ -18,7 +18,7 @@ setup(
   author_email='mario1842.info@gmail.com',
   license='MIT', 
   classifiers=classifiers,
-  keywords='discord level card', 
+  keywords='discord level card welcome',
   packages=find_packages(),
   install_requires=['Pillow','easy-pil', 'discord.py'] 
 )
